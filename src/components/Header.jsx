c444720cff1e79381b7c08798830cacdc10a0d13
jelena <jelenablagovcanin99@gmail.com>
2023-04-28 12:22:20 +0200

    header and add button

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+export default function Header() {
+  return (
+    <Box  sx={{flexGrow: 1}}>
+      <AppBar position="static" variant="secondary">
+        <Toolbar>
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1}}>
+            Notes
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
---
+++ src/components/NotesList.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material"
+
+export default function NotesList() {
+  return (
+  <>
+<Button variant="outlined">
+Add new Note
+</Button>
+
+  </>
+  )
+}
